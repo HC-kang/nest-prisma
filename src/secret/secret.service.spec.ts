a38281ce97d8dfd1fc2c09e6505e2842bd6ee84f
HC-kang <weston0713@gmail.com>
2023-06-11 12:27:11 +0900

    Test: add secret service tests

--- src/secret/secret.service.spec.ts
+++ src/secret/secret.service.spec.ts
@@ -1,18 +1,121 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SecretService } from './secret.service';
+import { SecretValidator } from './secret.validator';
+import { SecretRepository } from './secret.repository';
+import { GetSecretParam } from './dto/get_secret.dto';
+import { Secret } from '@prisma/client';
+import { CreateSecretBody } from './dto/create_secret.dto';
+import { DeleteSecretParam } from './dto/delete_secret.dto';
 
 describe('SecretService', () => {
-  let service: SecretService;
+  let secretService: SecretService;
+  let secretValidator: SecretValidator;
+  let mockSecretRepository: Partial<SecretRepository>;
 
   beforeEach(async () => {
+    mockSecretRepository = {};
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SecretService],
+      providers: [
+        SecretService,
+        SecretValidator,
+        {
+          provide: SecretRepository,
+          useValue: mockSecretRepository,
+        },
+      ],
     }).compile();
 
-    service = module.get<SecretService>(SecretService);
+    secretService = module.get<SecretService>(SecretService);
+    secretValidator = module.get<SecretValidator>(SecretValidator);
+    mockSecretRepository = module.get<SecretRepository>(SecretRepository);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(secretService).toBeDefined();
+  });
+
+  describe('getSecret Test', () => {
+    it('should return a secret', async () => {
+      // Arrange
+      const param: GetSecretParam = {
+        urlId: '123456qwerty',
+      };
+      const mockSecret: Secret = {
+        urlId: '123456qwerty',
+        createdAt: undefined,
+        updatedAt: undefined,
+        deletedAt: undefined,
+        secret: 'myValidSecret',
+      };
+      const secretFindUniqueArgs = secretValidator.getSecretValidator(param);
+      mockSecretRepository.getSecret = jest.fn().mockReturnValue(mockSecret);
+
+      // Act
+      const result = await secretService.getSecret(param);
+
+      // Assert
+      expect(result).toEqual(mockSecret);
+      expect(mockSecretRepository.getSecret).toBeCalledTimes(1);
+      expect(mockSecretRepository.getSecret).toBeCalledWith(
+        secretFindUniqueArgs,
+      );
+    });
+  });
+
+  describe('createSecret Test', () => {
+    it('should create and return a secret', async () => {
+      // Arrange
+      const mockBody: CreateSecretBody = {
+        urlId: '123456qwerty',
+        secret: 'myValidSecret',
+      };
+      const mockSecret: Secret = {
+        urlId: '123456qwerty',
+        createdAt: undefined,
+        updatedAt: undefined,
+        deletedAt: undefined,
+        secret: 'myValidSecret',
+      };
+      const secretCreateArgs = secretValidator.createSecretValidator(mockBody);
+      mockSecretRepository.createSecret = jest.fn().mockReturnValue(mockSecret);
+
+      // Act
+      const result = await secretService.createSecret(mockBody);
+
+      // Assert
+      expect(result).toEqual(mockSecret);
+      expect(mockSecretRepository.createSecret).toBeCalledTimes(1);
+      expect(mockSecretRepository.createSecret).toBeCalledWith(
+        secretCreateArgs,
+      );
+    });
+  });
+
+  describe('deleteSecret Test', () => {
+    it('should delete and return a secret', async () => {
+      // Arrange
+      const param: DeleteSecretParam = {
+        urlId: '123456qwerty',
+      };
+      const mockSecret: Secret = {
+        urlId: '123456qwerty',
+        createdAt: undefined,
+        updatedAt: undefined,
+        deletedAt: undefined,
+        secret: 'myValidSecret',
+      };
+      const secretDeleteInput = secretValidator.deleteSecretValidator(param);
+      mockSecretRepository.deleteSecret = jest.fn().mockReturnValue(mockSecret);
+
+      // Act
+      const result = await secretService.deleteSecret(param);
+
+      // Assert
+      expect(result).toEqual(mockSecret);
+      expect(mockSecretRepository.deleteSecret).toBeCalledTimes(1);
+      expect(mockSecretRepository.deleteSecret).toBeCalledWith(
+        secretDeleteInput,
+      );
+    });
   });
 });
--- src/secret/secret.service.ts
+++ src/secret/secret.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { SecretRepository } from './secret.repository';
+import { SecretValidator } from './secret.validator';
+import { Secret } from '@prisma/client';
+import { GetSecretParam } from './dto/get_secret.dto';
+import { CreateSecretBody } from './dto/create_secret.dto';
 
 @Injectable()
 export class SecretService {
-  // constructor(
-  //   private secretRepository: SecretRepository,
-  //   private secretValidator: SecretValidator,
-  // ) {}
+  constructor(
+    private secretRepository: SecretRepository,
+    private secretValidator: SecretValidator,
+  ) {}
+
+  async getSecret(param: GetSecretParam): Promise<Secret> {
+    return await this.secretRepository.getSecret(
+      this.secretValidator.getSecretValidator(param),
+    );
+  }
+
+  async createSecret(body: CreateSecretBody): Promise<Secret> {
+    return await this.secretRepository.createSecret(
+      this.secretValidator.createSecretValidator(body),
+    );
+  }
+
+  async deleteSecret(param: GetSecretParam): Promise<Secret> {
+    return await this.secretRepository.deleteSecret(
+      this.secretValidator.deleteSecretValidator(param),
+    );
+  }
 }
