0bd4fa5906ce646421124767ec667ccd9b899b72
HC-kang <weston0713@gmail.com>
2023-06-18 17:04:15 +0900

    Remove: remove app service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { SecretModule } from './secret/secret.module';
@@ -8,7 +6,5 @@ import { UtilModule } from './util/util.module';
 
 @Module({
   imports: [PrismaModule, UserModule, SecretModule, UtilModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
