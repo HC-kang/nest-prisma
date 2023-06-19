19fe37665e2199f6e5a3cc31baf572f473d51436
HC-kang <weston0713@gmail.com>
2023-06-19 14:04:23 +0900

    Feat: init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
-import { UserModule } from './user/user.module';
-import { SecretModule } from './secret/secret.module';
 import { UtilModule } from './util/util.module';
-import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, SecretModule, UtilModule, PostModule],
+  imports: [UtilModule],
 })
 export class AppModule {}
--- src/secret/dto/delete_secret.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class DeleteSecretParam {
-  @IsString()
-  @IsNotEmpty()
-  urlId: string;
-}
