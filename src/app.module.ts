09eb4207284a35eca710e6602dbb40e9ce527c35
HC-kang <weston0713@gmail.com>
2023-06-20 09:46:54 +0900

    Feat: add user model and ClassSerializerInterceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UtilModule } from './util/util.module';
 import { ArticlesModule } from './articles/articles.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UtilModule, ArticlesModule],
+  imports: [UtilModule, ArticlesModule, UsersModule],
 })
 export class AppModule {}
