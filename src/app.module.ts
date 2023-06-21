d5d16afe3f5eadb5290a7d18f324e83bf0bedf57
HC-kang <weston0713@gmail.com>
2023-06-21 10:55:19 +0900

    Fix: add injectable decorator to jwt strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UtilModule } from './util/util.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [UtilModule, UsersModule, AuthModule],
+  imports: [UtilModule, UsersModule, AuthModule, PostsModule],
 })
 export class AppModule {}
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,13 +2,11 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { jwtSecret } from './auth.module';
 import { UsersService } from '@src/users/users.service';
 import { PassportStrategy } from '@nestjs/passport';
-import { UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(private usersService: UsersService) {
-    console.log('usersService@@@@@@@@@@@');
-    console.log(usersService);
-    console.log('usersService@@@@@@@@@@@');
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: jwtSecret,
